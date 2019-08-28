d192f5b25d5ea630c7fdd83b7e6f55689282be0c
Piou-piou <pilloud.anthony@gmail.com>
2019-08-28 07:48:06 +0200

    [func] add contacts methods

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Service\FileTreaterFineUploader;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PagesController extends AbstractController
@@ -39,5 +43,37 @@ class PagesController extends AbstractController
     public function about()
     {
         return $this->render("pages/about.html.twig");
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render("pages/contact.html.twig");
+    }
+
+    /**
+     * @Route("/contact/send", name="contact_send")
+     * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @return RedirectResponse
+     */
+    public function sendForm(Request $request, Swift_Mailer $mailer): RedirectResponse
+    {
+        $message = "<h2> Message de la part de  :" . $request->get("firstname") . " " . $request->get("lastname") . "</h2><br><br>";
+
+        $mail = $message . $request->get("message");
+
+        $message = (new Swift_Message("Message de margauxbailly.fr, sujet : " . $request->get("object")))
+            ->setFrom("no-reply@onlineformapro.com")
+            ->setReplyTo($request->get("email"))
+            ->setTo("margaux.bailly.design@gmail.com")
+            ->setBody($mail, "text/html");
+
+        $mailer->send($message);
+        $this->addFlash("success", "Votre message a été envoyé");
+
+        return $this->redirectToRoute("contact");
     }
 }
